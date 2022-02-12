4741e65646d9e9993e7954b08bc0db9c566e6878
music-discussion <jadonbaker@icloud.com>
2022-02-12 16:10:20 -0600

    Added Psych Engine Shit Support

--- source/Character.hx
+++ source/Character.hx
@@ -20,6 +20,30 @@ import lime.utils.Assets;
 
 using StringTools;
 
+typedef CharacterFile = {
+	var animations:Array<AnimArray>;
+	var image:String;
+	var scale:Float;
+	var sing_duration:Float;
+	var healthicon:String;
+
+	var position:Array<Float>;
+	var camera_position:Array<Float>;
+
+	var flip_x:Bool;
+	var no_antialiasing:Bool;
+	var healthbar_colors:Array<Int>;
+}
+
+typedef AnimArray = {
+	var anim:String;
+	var name:String;
+	var fps:Int;
+	var loop:Bool;
+	var indices:Array<Int>;
+	var offsets:Array<Int>;
+}
+
 class Character extends FlxSprite
 {
 	public var animOffsets:Map<String, Array<Dynamic>>;
@@ -42,6 +66,7 @@ class Character extends FlxSprite
 	public var midpointY:Int = 0;
 	public var isDie:Bool = false;
 	public var isPixel:Bool = false;
+	public var isPsychFile:Bool = false;
 
 	public var holdTimer:Float = 0;
 
@@ -52,6 +77,32 @@ class Character extends FlxSprite
 	public var tex:FlxAtlasFrames;
 
 	public var playerOffset:Array<String>;
+
+	public var colorTween:FlxTween;
+	public var holdTimer:Float = 0;
+	public var heyTimer:Float = 0;
+	public var specialAnim:Bool = false;
+	public var animationNotes:Array<Dynamic> = [];
+	public var stunned:Bool = false;
+	public var singDuration:Float = 4; //Multiplier of how long a character holds the sing pose
+	public var idleSuffix:String = '';
+	public var danceIdle:Bool = false; //Character use "danceLeft" and "danceRight" instead of "idle"
+	
+
+	public var healthIcon:String = 'face';
+	public var animationsArray:Array<AnimArray> = [];
+
+	public var positionArray:Array<Float> = [0, 0];
+	public var cameraPosition:Array<Float> = [0, 0];
+
+	public var hasMissAnimations:Bool = false;
+
+	//Used on Character Editor
+	public var imageFile:String = '';
+	public var jsonScale:Float = 1;
+	public var noAntialiasing:Bool = false;
+	public var originalFlipX:Bool = false;
+	public var healthColorArray:Array<Int> = [255, 0, 0];
 
 	public function new(x:Float, y:Float, ?character:String = "bf", ?isPlayer:Bool = false)
 	{
@@ -6953,7 +7004,143 @@ class Character extends FlxSprite
 				}		
 	
 				playAnim('idle');
+			default:
+				ifPsychFile = true;
+				var characterPath:String = 'characters/' + curCharacter + '.json';
+				#if MODS_ALLOWED
+				
+				
+				
+				
+				var path:String = Paths.modFolders(characterPath);
+				if (!FileSystem.exists(path)) {
+					path = Paths.getPreloadPath(characterPath);
+				}
 
+				if (!FileSystem.exists(path))
+				
+				
+				
+				#else
+				var path:String = Paths.getPreloadPath(characterPath);
+				if (!Assets.exists(path))
+				#end
+				{
+					path = Paths.getPreloadPath('characters/' + DEFAULT_CHARACTER + '.json'); //If a character couldn't be found, change him to BF just to prevent a crash
+				}
+
+				#if MODS_ALLOWED
+				var rawJson = File.getContent(path);
+				#else
+				var rawJson = Assets.getText(path);
+				#end
+
+				var json:CharacterFile = cast Json.parse(rawJson);
+				var spriteType = "sparrow";
+				//sparrow
+				//packer
+				//texture
+				#if MODS_ALLOWED
+				var modTxtToFind:String = Paths.modsTxt(json.image);
+				var txtToFind:String = Paths.getPath('images/' + json.image + '.txt', TEXT);
+				
+				//var modTextureToFind:String = Paths.modFolders("images/"+json.image);
+				//var textureToFind:String = Paths.getPath('images/' + json.image, new AssetType();
+				
+				if (FileSystem.exists(modTxtToFind) || FileSystem.exists(txtToFind) || Assets.exists(txtToFind))
+				#else
+				if (Assets.exists(Paths.getPath('images/' + json.image + '.txt', TEXT)))
+				#end
+				{
+					
+					spriteType = "packer";
+					
+				}
+				
+				
+				
+				
+				#if MODS_ALLOWED
+				var modAnimToFind:String = Paths.modFolders('images/' + json.image + '/Animation.json');
+				var animToFind:String = Paths.getPath('images/' + json.image + '/Animation.json', TEXT);
+				
+				//var modTextureToFind:String = Paths.modFolders("images/"+json.image);
+				//var textureToFind:String = Paths.getPath('images/' + json.image, new AssetType();
+				
+				if (FileSystem.exists(modAnimToFind) || FileSystem.exists(animToFind) || Assets.exists(animToFind))
+				#else
+				if (Assets.exists(Paths.getPath('images/' + json.image + '/Animation.json', TEXT)))
+				#end
+				{
+					
+					spriteType = "texture";
+					
+				}
+				
+				
+				
+				
+				switch (spriteType){
+					
+					case "packer":
+						frames = Paths.getPackerAtlas(json.image);
+					
+					case "sparrow":
+						frames = Paths.getSparrowAtlas(json.image);
+					
+					case "texture":
+						frames = AtlasFrameMaker.construct(json.image);
+						
+						
+				}
+				
+				imageFile = json.image;
+
+				if(json.scale != 1) {
+					jsonScale = json.scale;
+					setGraphicSize(Std.int(width * jsonScale));
+					updateHitbox();
+				}
+
+				positionArray = json.position;
+				cameraPosition = json.camera_position;
+
+				healthIcon = json.healthicon;
+				singDuration = json.sing_duration;
+				flipX = !!json.flip_x;
+				if(json.no_antialiasing) {
+					antialiasing = false;
+					noAntialiasing = true;
+				}
+
+				if(json.healthbar_colors != null && json.healthbar_colors.length > 2)
+					healthColorArray = json.healthbar_colors;
+
+				antialiasing = !noAntialiasing;
+				if(!ClientPrefs.globalAntialiasing) antialiasing = false;
+
+				animationsArray = json.animations;
+				if(animationsArray != null && animationsArray.length > 0) {
+					for (anim in animationsArray) {
+						var animAnim:String = '' + anim.anim;
+						var animName:String = '' + anim.name;
+						var animFps:Int = anim.fps;
+						var animLoop:Bool = !!anim.loop; //Bruh
+						var animIndices:Array<Int> = anim.indices;
+						if(animIndices != null && animIndices.length > 0) {
+							animation.addByIndices(animAnim, animName, animIndices, "", animFps, animLoop);
+						} else {
+							animation.addByPrefix(animAnim, animName, animFps, animLoop);
+						}
+
+						if(anim.offsets != null && anim.offsets.length > 1) {
+							addOffset(anim.anim, anim.offsets[0], anim.offsets[1]);
+						}
+					}
+				} else {
+					quickAnimAdd('idle', 'BF idle dance');
+				}
+				//trace('Loaded file to character ' + curCharacter);
 		}
 
 		if(animation.getByName('danceLeft') != null && animation.getByName('danceRight') != null)
@@ -6961,7 +7148,8 @@ class Character extends FlxSprite
 			danceIdle = true;
 		}
 
-		dance();
+		switch (isPsychFile) {
+			case false:
 
 		if (isPlayer)
 		{
@@ -7016,10 +7204,44 @@ class Character extends FlxSprite
 				}
 			}
 		}
+		case true:
+			originalFlipX = flipX;
+
+		if(animOffsets.exists('singLEFTmiss') || animOffsets.exists('singDOWNmiss') || animOffsets.exists('singUPmiss') || animOffsets.exists('singRIGHTmiss')) hasMissAnimations = true;
+		recalculateDanceIdle();
+		dance();
+
+		if (isPlayer)
+		{
+			flipX = !flipX;
+
+			/*// Doesn't flip for BF, since his are already in the right place???
+			if (!curCharacter.startsWith('bf'))
+			{
+				// var animArray
+				if(animation.getByName('singLEFT') != null && animation.getByName('singRIGHT') != null)
+				{
+					var oldRight = animation.getByName('singRIGHT').frames;
+					animation.getByName('singRIGHT').frames = animation.getByName('singLEFT').frames;
+					animation.getByName('singLEFT').frames = oldRight;
+				}
+
+				// IF THEY HAVE MISS ANIMATIONS??
+				if (animation.getByName('singLEFTmiss') != null && animation.getByName('singRIGHTmiss') != null)
+				{
+					var oldMiss = animation.getByName('singRIGHTmiss').frames;
+					animation.getByName('singRIGHTmiss').frames = animation.getByName('singLEFTmiss').frames;
+					animation.getByName('singLEFTmiss').frames = oldMiss;
+				}
+			}*/
+		}
+	}
 	}
 
 	override function update(elapsed:Float)
 	{
+		switch (isPsychFile) {
+			case false:
 		if (!isPlayer)
 		{
 			if (animation.curAnim.name.startsWith('sing'))
@@ -7047,6 +7269,47 @@ class Character extends FlxSprite
 				if (animation.curAnim.name == 'hairFall' && animation.curAnim.finished)
 					playAnim('danceRight');
 		}
+		case true: 
+			if(!debugMode && animation.curAnim != null)
+				{
+					if(heyTimer > 0)
+					{
+						heyTimer -= elapsed;
+						if(heyTimer <= 0)
+						{
+							if(specialAnim && animation.curAnim.name == 'hey' || animation.curAnim.name == 'cheer')
+							{
+								specialAnim = false;
+								dance();
+							}
+							heyTimer = 0;
+						}
+					} else if(specialAnim && animation.curAnim.finished)
+					{
+						specialAnim = false;
+						dance();
+					}
+		
+					if (!isPlayer)
+					{
+						if (animation.curAnim.name.startsWith('sing'))
+						{
+							holdTimer += elapsed;
+						}
+		
+						if (holdTimer >= Conductor.stepCrochet * 0.001 * singDuration)
+						{
+							dance();
+							holdTimer = 0;
+						}
+					}
+		
+					if(animation.curAnim.finished && animation.getByName(animation.curAnim.name + '-loop') != null)
+					{
+						playAnim(animation.curAnim.name + '-loop');
+					}
+				}
+	}
 
 		super.update(elapsed);
 	}
@@ -7057,7 +7320,8 @@ class Character extends FlxSprite
 	 * FOR GF DANCING SHIT
 	 */
 	public function dance()
-	{
+	{switch (isPsychFile) {
+		case false:
 		if (!debugMode)
 		{
 			if (danceIdle)
@@ -7096,6 +7360,23 @@ class Character extends FlxSprite
 				color = FlxColor.WHITE;
 			}
 		}
+		case true:
+			if (!debugMode && !specialAnim)
+				{
+					if(danceIdle)
+					{
+						danced = !danced;
+		
+						if (danced)
+							playAnim('danceRight' + idleSuffix);
+						else
+							playAnim('danceLeft' + idleSuffix);
+					}
+					else if(animation.getByName('idle' + idleSuffix) != null) {
+							playAnim('idle' + idleSuffix);
+					}
+				}
+	}
 	}
 
 	public function setZoom(?toChange:Float = 1):Void
@@ -7106,6 +7387,8 @@ class Character extends FlxSprite
 
 	public function playAnim(AnimName:String, Force:Bool = false, Reversed:Bool = false, Frame:Int = 0):Void
 	{
+		switch (isPsychFile) {
+		case false:
 		var missed:Bool = false;
 
 		if (AnimName.endsWith('alt') && animation.getByName(AnimName) == null)
@@ -7152,10 +7435,48 @@ class Character extends FlxSprite
 				danced = !danced;
 			}
 		}
+		case true: 
+			specialAnim = false;
+		animation.play(AnimName, Force, Reversed, Frame);
+
+		var daOffset = animOffsets.get(AnimName);
+		if (animOffsets.exists(AnimName))
+		{
+			offset.set(daOffset[0], daOffset[1]);
+		}
+		else
+			offset.set(0, 0);
+
+		if (curCharacter.startsWith('gf'))
+		{
+			if (AnimName == 'singLEFT')
+			{
+				danced = true;
+			}
+			else if (AnimName == 'singRIGHT')
+			{
+				danced = false;
+			}
+
+			if (AnimName == 'singUP' || AnimName == 'singDOWN')
+			{
+				danced = !danced;
+			}
+		}
+	}
 	}
 
 	public function addOffset(name:String, x:Float = 0, y:Float = 0)
 	{
 		animOffsets[name] = [x, y];
+	}
+
+	public function recalculateDanceIdle() {
+		danceIdle = (animation.getByName('danceLeft' + idleSuffix) != null && animation.getByName('danceRight' + idleSuffix) != null);
+	}
+
+	public function quickAnimAdd(name:String, anim:String)
+	{
+		animation.addByPrefix(name, anim, 24, false);
 	}
 }
