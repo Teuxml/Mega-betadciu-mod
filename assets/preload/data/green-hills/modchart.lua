4d0ebf7a075557ed5a2037bc83dd8524d4cc196a
Teuxml <85766646+Teuxml@users.noreply.github.com>
2022-02-13 21:08:00 -0300

    Update modchart.lua

--- assets/preload/data/green-hills/modchart.lua
+++ assets/preload/data/green-hills/modchart.lua
@@ -25,71 +25,127 @@ end
 function stepHit (step)
 
     if curStep == 233 then -- steven
-        changeDadCharacter('steven', 150, 520)
+        changeDadCharacter('steven', 100, 100)
     end
 
     if curStep == 319 then -- finn
-        changeBoyfriendCharacter('mom', 1120, 480)   
+        changeBoyfriendCharacter('mom', 770, 450)   
     end
 
     if curStep == 383 then -- glitch teu
-        changeDadCharacter('glitch-teu', 150, 520)
+        changeDadCharacter('glitch-teu', 100, 100)
     end
 
     if curStep == 446 then -- old teu
-        changeBoyfriendCharacter('teu-old-demon', 1120, 480)   
+        changeBoyfriendCharacter('teu-old-demon', 770, 450)   
     end
 
     if curStep == 510 then -- mickey
-        changeDadCharacter('happymouse', 150, 520)
+        changeDadCharacter('happymouse', 100, 100)
     end
 
     if curStep == 573 then -- oswald
-        changeBoyfriendCharacter('oswald-happy', 1120, 480)   
+        changeBoyfriendCharacter('oswald-happy', 770, 450)   
     end
 
     if curStep == 636 then -- bambi
-        changeDadCharacter('bambi', 150, 520)
+        changeDadCharacter('bambi', 100, 100)
     end
 
     if curStep == 669 then -- 69, nice (dave)
-        changeBoyfriendCharacter('dave', 1120, 480)   
+        changeBoyfriendCharacter('dave', 770, 450)   
     end
 
     if curStep == 702 then -- opheebop
-        changeDadCharacter('opheebop', 150, 520)
+        changeDadCharacter('opheebop', 100, 100)
     end
 
     if curStep == 733 then -- bob
-        changeBoyfriendCharacter('bob', 1120, 480)   
+        changeBoyfriendCharacter('bob', 770, 450)   
     end
 
     if curStep == 749 then -- bob gets pissed at ron
-        changeBoyfriendCharacter('angrybob', 1120, 480)   
+        changeBoyfriendCharacter('angrybob', 770, 450)   
     end
 
     if curStep == 749 then -- ron
-        changeDadCharacter('ron', 1120, 480)   
+        changeDadCharacter('ron', 770, 450)   
     end
 
     if curStep == 812 then -- tray or train?
-        changeDadCharacter('shaggy', 1120, 480)   
+        changeDadCharacter('shaggy', 770, 450)   
     end
 
     if curStep == 828 then -- lucas
-        changeDadCharacter('lucas', 150, 520)
+        changeDadCharacter('lucas', 100, 100)
     end
 
     if curStep == 828 then -- daniel
-        changeBoyfriendCharacter('daniel', 1120, 480)   
+        changeBoyfriendCharacter('daniel', 770, 450)   
     end
 
     if curStep == 828 then -- fly
-        changeDadCharacter('fly', 150, 520)
+        changeDadCharacter('fly', 100, 100)
     end
 
     if curStep == 828 then -- mathias
-        changeBoyfriendCharacter('mathias', 1120, 480)   
+        changeBoyfriendCharacter('mathias', 770, 450)   
+    end
+
+    if curStep == 1018 then -- TGT Sonic
+        changeDadCharacter('sonic', 100, 100)
+    end
+
+    if curStep == 1083 then -- TGT Tails
+        changeBoyfriendCharacter('tails', 770, 450)   
+    end
+
+    if curStep == 1146 then -- Expurtesco man
+        changeDadCharacter('TescoExpurgation', 100, 100)
+    end
+
+    if curStep == 1162 then -- Expurtesco woman
+        changeBoyfriendCharacter('TescoExpurgation', 770, 450)   
+    end
+
+    if curStep == 1210 then -- Sans nutdealer
+        changeDadCharacter('sans', 100, 100)
+    end
+
+    if curStep == 1210 then -- penumbra
+        changeBoyfriendCharacter('penumbra', 770, 450)   
+    end
+
+    if curStep == 1210 then -- The awesome musician himself (but dead version)
+        changeDadCharacter('JadynPibby', 100, 100)
+    end
+
+    if curStep == 1210 then -- lowercase swordcube
+        changeBoyfriendCharacter('Sword', 770, 450)   
+    end
+
+    if curStep == 1400 then -- barkey
+        changeDadCharacter('barkey', 100, 100)
+    end
+
+    if curStep == 1400 then -- borky
+        changeBoyfriendCharacter('borky', 770, 450)   
+    end
+
+    if curStep == 1400 then -- barkey
+        changeDadCharacter('barkey', 100, 100)
+    end
+
+    if curStep == 1400 then -- borky
+        changeBoyfriendCharacter('borky', 770, 450)   
+    end
+
+    if curStep == 1532 then -- gemamugen
+        changeDadCharacter('gemamugen', 100, 100)
+    end
+
+    if curStep == 1660 then -- miner
+        changeBoyfriendCharacter('mineirinho', 770, 450)   
     end
 
 end
